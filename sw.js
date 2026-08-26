f3f270feeabb9bd3f8388e3793f4b3bcf3cb0251
ASP-Seyfors <thomas@alliedsurgicalproducts.com>
2026-08-25 21:54:51 -0400

    4.0.4

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * Copyright (c) 2026 Thomas Paul Seyfors / Allied Surgical Products.
  * All Rights Reserved.
  * ======================================================================= */
-const CACHE_NAME = 'asp-ims-v4.0.3';
+const CACHE_NAME = 'asp-ims-v4.0.4';
 
 
 const ASSETS_TO_CACHE = [
