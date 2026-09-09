5c2375ad06d3cc73eb045ee0bcec2c0a027f9e2c
ASP-Seyfors <thomas@alliedsurgicalproducts.com>
2026-09-08 23:03:52 -0400

    4.6.8

--- js/sessionManager.js
+++ js/sessionManager.js
@@ -48,6 +48,7 @@ const SessionManager = {
   googleFeederUrl: "https://script.google.com/macros/s/AKfycbxccIizG_pkX6ARslZCv4ElewSCRz_HUtsn0R8CKpCAFgVKPj972RLrL5eUsTNArq6IeA/exec",
 
   // Sandbox URLs
+  /*
   testArchiveUrl: "https://script.google.com/macros/s/AKfycbwHNk0QL0Cu1bInJxjDqGxvQ-RdPD8xJaPVV3OpTdAOVJZBhppqieVj7AKS_j2B5QpuBQ/exec", 
   testFeederUrl: "https://script.google.com/macros/s/AKfycbyugIbNY6XYvume81EmEklb32uFxuesGE9et3XrImUjYtHjkbKR6Q6goIHABAZ0P0BQfg/exec", 
 
@@ -59,6 +60,15 @@ const SessionManager = {
   getActiveFeederUrl() {
     let chk = document.getElementById('chkSandboxMode');
     return (chk && chk.checked) ? (this.testFeederUrl || this.googleFeederUrl) : this.googleFeederUrl;
+  },
+  */
+
+  getActiveArchiveUrl() {
+    return this.cloudArchiveUrl;
+  },
+  
+  getActiveFeederUrl() {
+    return this.googleFeederUrl;
   },
 
   applyTestingModeVisuals(isTestActive) {
