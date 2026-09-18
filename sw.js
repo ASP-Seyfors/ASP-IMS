2dcfcf96c6481a2cdf9a84dd47713fe53ec1b5f1
ASP-Seyfors <thomas@alliedsurgicalproducts.com>
2026-09-18 01:05:23 -0400

    PROD environment updates

--- sw.js
+++ sw.js
@@ -17,16 +17,16 @@
  * Copyright (c) 2026 Thomas Seyfors / Allied Surgical Products.
  * All Rights Reserved.
  * ======================================================================= */
-const CACHE_NAME = 'asp-ims-demo-v5.1.5';
+const CACHE_NAME = 'asp-ims-v5.1.5';
 
 
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './database.json',
-  './ASP_Box_Web_RGB_DEMO.png',
-  './ASP_Icon_192_DEMO.png',
-  './ASP_Icon_512_DEMO.png',
+  './ASP_Box_Web_RGB.png',
+  './ASP_Icon_192.png',
+  './ASP_Icon_512.png',
   
   // CSS
   './css/layout.css',
